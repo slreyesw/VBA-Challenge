4818a03624590dff1e69935a2301d03d53a3f529
slreyesw <shelby.reyes01@gmail.com>
2023-05-11 14:44:27 -0500

    $git commit

--- vba challenge.vb
+++ vba challenge.vb
@@ -10,67 +10,66 @@ Sub stock_analysis():
     Dim days As Integer
     Dim dailyChange As Double
     Dim averageChange As Double
-    Dim ws As Worksheet
     For Each ws In Worksheets
     ' Set title row
-    Range("I1").Value = "Ticker"
-    Range("J1").Value = "Yearly Change"
-    Range("K1").Value = "Percent Change"
-    Range("L1").Value = "Total Stock Volume"
-    Range("P1").Value = "Ticker"
-    Range("Q1").Value = "Value"
-    Range("O2").Value = "Greatest % Increase"
-    Range("O3").Value = "Greatest % Decrease"
-    Range("O4").Value = "Greatest Total Volume"
+    ws.Range("I1").Value = "Ticker"
+    ws.Range("J1").Value = "Yearly Change"
+    ws.Range("K1").Value = "Percent Change"
+    ws.Range("L1").Value = "Total Stock Volume"
+    ws.Range("P1").Value = "Ticker"
+    ws.Range("Q1").Value = "Value"
+    ws.Range("O2").Value = "Greatest % Increase"
+    ws.Range("O3").Value = "Greatest % Decrease"
+    ws.Range("O4").Value = "Greatest Total Volume"
     ' Set initial values
     j = 0
     total = 0
     change = 0
     start = 2
     ' get the row number of the last row with data
-    rowCount = Cells(Rows.Count, "A").End(xlUp).Row
+    rowCount = ws.Cells(Rows.Count, "A").End(xlUp).Row
     For i = 2 To rowCount
         ' If ticker changes then print results
-        If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
+        If ws.Cells(i + 1, 1).Value <> ws.Cells(i, 1).Value Then
             ' Stores results in variables
-            total = total + Cells(i, 7).Value
+            total = total + ws.Cells(i, 7).Value
             ' Handle zero total volume
             If total = 0 Then
                 ' print the results
-                Range("I" & 2 + j).Value = Cells(i, 1).Value
-                Range("J" & 2 + j).Value = 0
-                Range("K" & 2 + j).Value = "%" & 0
-                Range("L" & 2 + j).Value = 0
+                ws.Range("I" & 2 + j).Value = ws.Cells(i, 1).Value
+                ws.Range("J" & 2 + j).Value = 0
+                ws.Range("K" & 2 + j).Value = "%" & 0
+                ws.Range("L" & 2 + j).Value = 0
             Else
                 ' Find First non zero starting value
-                If Cells(start, 3) = 0 Then
+                If ws.Cells(start, 3) = 0 Then
                     For find_value = start To i
-                        If Cells(find_value, 3).Value <> 0 Then
+                        If ws.Cells(find_value, 3).Value <> 0 Then
                             start = find_value
                             Exit For
                         End If
                      Next find_value
                 End If
                 ' Calculate Change
-                change = (Cells(i, 6) - Cells(start, 3))
-                percentChange = change / Cells(start, 3)
+                change = (ws.Cells(i, 6) - ws.Cells(start, 3))
+                percentChange = change / ws.Cells(start, 3)
                 ' start of the next stock ticker
                 start = i + 1
                 ' print the results
-                Range("I" & 2 + j).Value = Cells(i, 1).Value
-                Range("J" & 2 + j).Value = change
-                Range("J" & 2 + j).NumberFormat = "0.00"
-                Range("K" & 2 + j).Value = percentChange
-                Range("K" & 2 + j).NumberFormat = "0.00%"
-                Range("L" & 2 + j).Value = total
+                ws.Range("I" & 2 + j).Value = ws.Cells(i, 1).Value
+                ws.Range("J" & 2 + j).Value = change
+                ws.Range("J" & 2 + j).NumberFormat = "0.00"
+                ws.Range("K" & 2 + j).Value = percentChange
+                ws.Range("K" & 2 + j).NumberFormat = "0.00%"
+                ws.Range("L" & 2 + j).Value = total
                 ' colors positives green and negatives red
                 Select Case change
                     Case Is > 0
-                        Range("J" & 2 + j).Interior.ColorIndex = 4
+                        ws.Range("J" & 2 + j).Interior.ColorIndex = 4
                     Case Is < 0
-                        Range("J" & 2 + j).Interior.ColorIndex = 3
+                        ws.Range("J" & 2 + j).Interior.ColorIndex = 3
                     Case Else
-                        Range("J" & 2 + j).Interior.ColorIndex = 0
+                        ws.Range("J" & 2 + j).Interior.ColorIndex = 0
                 End Select
             End If
             ' reset variables for new stock ticker
@@ -80,20 +79,20 @@ Sub stock_analysis():
             days = 0
         ' If ticker is still the same add results
         Else
-            total = total + Cells(i, 7).Value
+            total = total + ws.Cells(i, 7).Value
         End If
     Next i
     ' take the max and min and place them in a separate part in the worksheet
-    Range("Q2") = "%" & WorksheetFunction.Max(Range("K2:K" & rowCount)) * 100
-    Range("Q3") = "%" & WorksheetFunction.Min(Range("K2:K" & rowCount)) * 100
-    Range("Q4") = WorksheetFunction.Max(Range("L2:L" & rowCount))
+    ws.Range("Q2") = "%" & WorksheetFunction.Max(ws.Range("K2:K" & rowCount)) * 100
+    ws.Range("Q3") = "%" & WorksheetFunction.Min(ws.Range("K2:K" & rowCount)) * 100
+    ws.Range("Q4") = WorksheetFunction.Max(ws.Range("L2:L" & rowCount))
     ' returns one less because header row not a factor
-    increase_number = WorksheetFunction.Match(WorksheetFunction.Max(Range("K2:K" & rowCount)), Range("K2:K" & rowCount), 0)
-    decrease_number = WorksheetFunction.Match(WorksheetFunction.Min(Range("K2:K" & rowCount)), Range("K2:K" & rowCount), 0)
-    volume_number = WorksheetFunction.Match(WorksheetFunction.Max(Range("L2:L" & rowCount)), Range("L2:L" & rowCount), 0)
+    increase_number = WorksheetFunction.Match(WorksheetFunction.Max(ws.Range("K2:K" & rowCount)), ws.Range("K2:K" & rowCount), 0)
+    decrease_number = WorksheetFunction.Match(WorksheetFunction.Min(ws.Range("K2:K" & rowCount)), ws.Range("K2:K" & rowCount), 0)
+    volume_number = WorksheetFunction.Match(WorksheetFunction.Max(ws.Range("L2:L" & rowCount)), ws.Range("L2:L" & rowCount), 0)
 ' final ticker symbol for  total, greatest % of increase and decrease, and average
-    Range("P2") = Cells(increase_number + 1, 9)
-    Range("P3") = Cells(decrease_number + 1, 9)
-    Range("P4") = Cells(volume_number + 1, 9)
+    ws.Range("P2") = ws.Cells(increase_number + 1, 9)
+    ws.Range("P3") = ws.Cells(decrease_number + 1, 9)
+    ws.Range("P4") = ws.Cells(volume_number + 1, 9)
 Next ws
 End Sub
